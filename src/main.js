81f7338c92b5a1deaea1da43c1a5b8efae35c35f
smallsun0122 <lizhuohua@wakedata.com>
2021-10-31 12:17:04 +0800

    update

--- src/main.js
+++ src/main.js
@@ -15,7 +15,6 @@ export default function (Vue, { router, head, isClient }) {
       }
     }
   })
-  // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
   Vue.use(ElementUI)
 }
